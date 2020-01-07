803f764bb5e5fb4d3e4cf7e50d32040d5a36f7e4
Josi Reis-West <josialexa85@gmail.com>
2020-01-07 16:41:14 -0600

    Completed steps 7 and 8

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -10,5 +10,19 @@ module.exports = {
 
         const response = await db.get_user_treasure(req.session.user.id)
         res.status(200).json(response)
+    },
+    addUserTreasure: async (req, res) => {
+        const {treasureURL} = req.body
+        const {id} = req.session.user
+        const db = req.app.get('db')
+
+        const response = await db.add_user_treasure(treasureURL, id)
+        res.status(200).json(response)
+    },
+    getAllTreasure: async (req, res) => {
+        const db = req.app.get('db')
+
+        const treasure = await db.get_all_treasure()
+        res.status(200).json(treasure)
     }
 }
--- server/index.js
+++ server/index.js
@@ -28,5 +28,7 @@ app.get('/auth/logout', ac.logout)
 
 app.get('/api/treasure/dragon', tc.dragonTreasure)
 app.get('/api/treasure/user', am.usersOnly, tc.getUserTreasure)
+app.post('/api/treasure/user', am.usersOnly, tc.addUserTreasure)
+app.get('/api/treasure/all', am.usersOnly, am.adminsOnly, tc.getAllTreasure)
 
 app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
